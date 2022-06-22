8153425f7cb2c58dc2102ad0e804147980320d13
Chiheb <chihebbenrajab@gmail.com>
2022-06-22 17:41:39 +0100

    changes on user entity

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -20,6 +20,7 @@ public class User implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
+	//comment for Git
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
